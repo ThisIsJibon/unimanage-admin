f46994ebc78bf688d0e0d0efce5f157d614260e8
Mehedi Hasan <jibonmh7@gmail.com>
2022-08-12 12:04:01 +0600

    minor change in ui

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
-
+import { MdWavingHand } from "react-icons/md";
+import {TiTime}  from "react-icons/ti";
+import { BiCool } from "react-icons/bi";
+import 'bootstrap/dist/css/bootstrap.css';
 export default function Home() {
   return (
     <div className="home">
-      <h1>Home</h1>
+      
+        <h1>Welcome to Admin Dashboard.</h1>
+        <h4>Make sure data are in correct format.</h4>
     </div>
+    
   );
 }
